a4302474922f746e34f23d1411029607144ef3df
leachst <stewart.leach@cba.com.au>
2015-03-17 18:04:15 +1100

    loading data via http call and fixing parseModel function call

--- src/angular/watchlistCtrl.js
+++ src/angular/watchlistCtrl.js
@@ -40,30 +40,40 @@ var app = angular.module('watchlist', ['ui.multiselect']);
 // });
 
 app.controller('WatchlistCtrl', function($scope, $http, $timeout) {
-  $scope.test = "foo";
+	$http.get('/src/data/watchlistMockData.json').
+	  success(function(data, status, headers, config) {
+		console.log("success, returned data.");
+		$scope.watchlistData = data;
+		console.log(status);
+		console.log(data);
+	  }).
+	  error(function(data, status, headers, config) {
+		console.log("error returned");
+		console.log(status);
+		console.log(data);
+	  });
 
- 
-  $scope.watchlistSelected = {};
-  $scope.watchlistData = [
-    {name: 'My Default watchlist 1', extra: 'foo'},
-	{name: 'Property Watchlist', extra: 'foo'},
-	{name: 'Insurance Watchlist', extra: 'bar'},
-	{name: 'Fixed Interest Watchlist', extra: 'foo'},
-	{name: 'Other insurance Watchlist', extra: '1234'},
-	{name: 'Curabitur tempus sollicitudin', extra: 'foo'},
-	{name: 'velit nec quam', extra: 'foo'},
-	{name: 'commodo dictum', extra: 'foo abc'},
-	{name: 'Financial list 2', extra: 'foo'},
-	{name: 'Curabitur tempus', extra: 'foo 123'},
-	{name: 'Vestibulum et orci', extra: 'foo'},
-	{name: 'porta sit amet felis', extra: 'foo'},
-	{name: 'Praesent tincidunt', extra: 'abc'},
-	{name: 'Suspendisse purus eros', extra: 'foo'},
-	{name: 'Sed maximus dignissi', extra: 'foo'},
-	{name: 'Nullam nisi massa', extra: 'foo'},
-	{name: 'Tincidunt euismod nisi', extra: 'foo'},
-	{name: 'In consectetur pretium', extra: 'foo'},
-	{name: 'Financial', extra: 'foo'},
-	{name: 'Miscellaneous list 123', extra: 'foo'}
-  ];
+	$scope.watchlistSelected = {};
+	// $scope.watchlistData = [
+		// {name: 'My Default watchlist 1', extra: 'foo'},
+		// {name: 'Property Watchlist', extra: 'foo'},
+		// {name: 'Insurance Watchlist', extra: 'bar'},
+		// {name: 'Fixed Interest Watchlist', extra: 'foo'},
+		// {name: 'Other insurance Watchlist', extra: '1234'},
+		// {name: 'Curabitur tempus sollicitudin', extra: 'foo'},
+		// {name: 'velit nec quam', extra: 'foo'},
+		// {name: 'commodo dictum', extra: 'foo abc'},
+		// {name: 'Financial list 2', extra: 'foo'},
+		// {name: 'Curabitur tempus', extra: 'foo 123'},
+		// {name: 'Vestibulum et orci', extra: 'foo'},
+		// {name: 'porta sit amet felis', extra: 'foo'},
+		// {name: 'Praesent tincidunt', extra: 'abc'},
+		// {name: 'Suspendisse purus eros', extra: 'foo'},
+		// {name: 'Sed maximus dignissi', extra: 'foo'},
+		// {name: 'Nullam nisi massa', extra: 'foo'},
+		// {name: 'Tincidunt euismod nisi', extra: 'foo'},
+		// {name: 'In consectetur pretium', extra: 'foo'},
+		// {name: 'Financial', extra: 'foo'},
+		// {name: 'Miscellaneous list 123', extra: 'foo'}
+	// ];
 });
